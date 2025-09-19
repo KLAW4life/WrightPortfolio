f78c2846e6ee89f30bc3979c2430a7fd695f029d
Kerene Wright <kerene789@gmail.com>
2025-09-18 20:43:03 -0400

    New version of trying to publish to github pages

--- vite.config.ts
+++ vite.config.ts
@@ -1,11 +1,33 @@
+// import { defineConfig } from "vite";
+// import react from "@vitejs/plugin-react-swc";
+// import path from "path";
+// import { componentTagger } from "lovable-tagger";
+
+// // // https://vitejs.dev/config/
+// export default defineConfig(({ mode }) => ({
+//   // base: 'https://klaw4life.github.io/',
+//   server: {
+//     host: "::",
+//     port: 8080,
+//   },
+//   plugins: [
+//     react(),
+//     mode === 'development' &&
+//     componentTagger(),
+//   ].filter(Boolean),
+//   resolve: {
+//     alias: {
+//       "@": path.resolve(__dirname, "./src"),
+//     },
+//   },
+// }));
+
 import { defineConfig } from "vite";
 import react from "@vitejs/plugin-react-swc";
 import path from "path";
 import { componentTagger } from "lovable-tagger";
 
-// https://vitejs.dev/config/
 export default defineConfig(({ mode }) => ({
-  base: 'https://KLAW4life.github.io/',
   server: {
     host: "::",
     port: 8080,
@@ -20,4 +42,9 @@ export default defineConfig(({ mode }) => ({
       "@": path.resolve(__dirname, "./src"),
     },
   },
+  base: '/WrightPortfolio/' // Replace with your GitHub repo name
 }));
+
+
+// vite.config.ts
+
